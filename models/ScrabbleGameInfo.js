edadae7d194a1e212a557a474c956fee65ae3d2d
KingLai23 <kingylai@gmail.com>
2022-09-08 20:28:53 -0400

    updated schema

--- models/ScrabbleGameInfo.js
+++ models/ScrabbleGameInfo.js
@@ -14,7 +14,6 @@ const scrabbleGameInfoSchema = new Schema({
         }],
         lostPoints: Number,
         lastLetters: [String],
-        finalScore: [String],
         otherPlayerTiles: {
             tiles: [String],
             score: Number
